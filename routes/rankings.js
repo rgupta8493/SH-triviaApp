c9a34fba1962b68eac1c9dc77be8dd540b5781e9
rgupta8493 <r.gupta8493@ufl.edu>
2016-11-19 21:02:22 -0500

    local fix in Leaderbaord

--- routes/rankings.js
+++ routes/rankings.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    var cursor = req.db.collection('users').find().sort({'local.score': -1}).toArray(function(err, doc) { //access the database, and sort users by score.
+    var cursor = req.db.collection('users').find().sort({'local.score': -1}).toArray(function(err, doc) { //access the db, and sort users by score in decreasing way.
         if (!err) {
             res.render('rankings', {"doc": doc});
         }
